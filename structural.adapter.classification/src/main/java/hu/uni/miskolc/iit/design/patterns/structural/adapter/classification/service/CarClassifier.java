9eea11d62c40b0a5adf2c0da15652d15bdf9b1ac
Zsolt Toth <tothzs87@gmail.com>
2017-03-06 09:44:44 +0100

    structural patterns adapter classification Train method has been added to CarClassifier

--- structural.adapter.classification/src/main/java/hu/uni/miskolc/iit/design/patterns/structural/adapter/classification/service/CarClassifier.java
+++ structural.adapter.classification/src/main/java/hu/uni/miskolc/iit/design/patterns/structural/adapter/classification/service/CarClassifier.java
@@ -1,8 +1,12 @@
 package hu.uni.miskolc.iit.design.patterns.structural.adapter.classification.service;
 
+import java.util.Map;
+
 import hu.uni.miskolc.iit.design.patterns.structural.adapter.classification.model.Car;
 
 public interface CarClassifier {
 
 	Car.CarAcceptability classify(Car car);
+	
+	void train(Map<Car, Car.CarAcceptability> trainingSet);
 }
